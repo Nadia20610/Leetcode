951cda33fdf0432f41900447928bba57b3c2cb19
Nadia Ahmed <nadia.ahmed.mohamed20@gmail.com>
2025-02-01 18:36:58 +0200

    Time: 680 ms (13.37%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select 
+    IFNULL(
+        (
+            select 
+                distinct salary 
+            from 
+                Employee
+            order by 
+                salary desc
+            limit 1 offset 1
+        ),
+        null
+    ) AS SecondHighestSalary;
